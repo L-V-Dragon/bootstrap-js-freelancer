2c9c054a938c96e7912092948983e2bf475a249d
L-V-Dragon <viotti37@gmail.com>
2022-11-18 15:25:35 +0100

    Es completato no bonus

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
-let codiciSconto = ["YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24"];
+let codiciSconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 
 
-function submitFun(event){
+function submitFun(event) {
     console.log("entrato in funzione");
+    let prezzoTotale = 0;
     event.preventDefault();
-    costo = calcoloPrezzo();
-    sconto = document.getElementById("DisCode").value;
-    let totale = applicaSconto(costo, sconto);
-    document.getElementById("prezzo").innerHTML = "Il prezzo finale è di " + totale.tofixed(1) + "€"
+    costo = calcoloPrezzo(prezzoTotale);
+    document.getElementById("prezzo").innerHTML = "Il prezzo finale è di " + applicaSconto(costo) + "€"
 }
 
 
-function applicaSconto(prezzoBase, codiceInserito) {
+function applicaSconto(prezzoBase) {
     let sconto = document.getElementById("DisCode").value;
-    for (sconto of codiciSconto) {
-        if (sconto == codiceInserito) {
+    for (let scontoUtente of codiciSconto){
+        if (scontoUtente == sconto) {
             document.getElementById("DisCode").classList.remove("text-danger")
-            let indiceSconto = codiciSconto.indexOf(sconto);
+            let indiceSconto = codiciSconto.indexOf(scontoUtente);
             codiciSconto.splice(indiceSconto, 1);
             return prezzoScontato = prezzoBase * 0.75;
-        } else if(sconto != codiceInserito) {
-            document.getElementById("DisCode").classList.add("text-danger")
-            alert("CODICE SCONTO NON VALIDO O INESISTENTE");
-            return prezzoBase;
         }
     }
+    
+    if (sconto == ""){
+        return prezzoBase;
+    }
+        document.getElementById("DisCode").classList.add("text-danger")
+        alert("CODICE SCONTO NON VALIDO O INESISTENTE");
+        return prezzoBase;
 }
 
 
-function calcoloPrezzo() {
+function calcoloPrezzo(prezzo) {
     let tipoLavoro = document.getElementById("Select").value;
     let tariffa = 0;
-
-    switch (tipoLavoro) {
-        case 1:
+    let oreLavoro = document.getElementById("Hours").value;
+    switch (true) {
+        case tipoLavoro == 1:
             tariffa = 20.5;
-            break;
+            return prezzo = (tariffa * oreLavoro).toFixed(2);
 
-        case 2:
+        case tipoLavoro == 2:
             tariffa = 15.3;
-            break;
+            return prezzo = (tariffa * oreLavoro).toFixed(2);
 
-        case 3:
+        case tipoLavoro == 3:
             tariffa = 33.6;
-            break;
+            return prezzo = (tariffa * oreLavoro).toFixed(2);
 
     }
-    let oreLavoro = document.getElementById("Hours").value;
-    prezzo = tipoLavoro * oreLavoro;
-    return prezzo;
 
 }
